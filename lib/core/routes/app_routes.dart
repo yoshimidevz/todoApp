5f38ef7769392c65fc567b33ad41db8bbae4390d
yoshimidevz <yoshimidev1@gmail.com>
2026-03-10 14:48:40 -0300

    feat: Criação de rotas para implementação de detalhes nas tarefas related #7

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../../features/todo/domain/entities/todo_entity.dart';
+import '../../features/todo/presentation/pages/todo_page.dart';
+
+abstract class AppRoutes {
+  static const String todo       = '/';
+  static const String todoDetail = '/todo-detail'; // ← novo
+}
+
+final appRouter = GoRouter(
+  initialLocation: AppRoutes.todo,
+  routes: [
+    GoRoute(
+      path: AppRoutes.todo,
+      builder: (context, state) => const TodoPage(),
+    ),
+    GoRoute(
+      path: AppRoutes.todoDetail,
+      builder: (context, state) {
+        final todo = state.extra as TodoEntity; // ← recebe a tarefa
+        // return TodoDetailPage(todo: todo);
+      },
+    ),
+  ],
+);
