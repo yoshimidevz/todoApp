554a5ebe44493a8e7e4fc735c1032c0ae3560179
yoshimidevz <yoshimidev1@gmail.com>
2026-04-02 22:30:19 -0300

    feat: Encapsulando material app no app providers e usando a nova DI (closes #32)

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,25 @@ import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart';
 import 'core/di/injection.dart';
 import 'core/routes/app_routes.dart';
+import 'core/di/app_providers.dart';
 
 void main() {
   setupDependencies();
   runApp(const MyApp());
 }
 
+// main.dart
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: AppTheme.light,
-      routerConfig: appRouter,
-      debugShowCheckedModeBanner: false,
+    return AppProviders(
+      child: MaterialApp.router(
+        theme: AppTheme.light,
+        routerConfig: appRouter,
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
